8ca1ac9123fc472f4a42585f213e3eabfdd1f8d6
Rahat Islam Akash <rahatislamakash@gmail.com>
2024-01-30 14:30:59 +0600

    created repository class

---
+++ app/src/main/java/com/example/todowithkotlin/NoteRepository.kt
@@ -0,0 +1,17 @@
+package com.example.todowithkotlin
+
+import androidx.lifecycle.LiveData
+
+class NoteRepository(private  val notesDao: NoteDao){
+    val allnotes: LiveData<MutableList<Note>> = notesDao.getAllNotes()
+
+    suspend fun insert(note: Note){
+        notesDao.insert(note)
+    }
+    suspend fun delete(note: Note){
+        notesDao.delete(note)
+    }
+    suspend fun update(note: Note){
+        notesDao.update(note)
+    }
+}
