9532c96cbdecd84a80069bd24f87137e946d5a0f
Rahat Islam Akash <rahatislamakash@gmail.com>
2024-01-30 13:58:05 +0600

    setup the room structure form my todo app

---
+++ app/src/main/java/com/example/todowithkotlin/Note.kt
@@ -0,0 +1,15 @@
+package com.example.todowithkotlin
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "note_table")
+data class Note(
+    val title: String,
+    val description: String,
+    val age: Int
+){
+    @PrimaryKey(autoGenerate = true)
+    val id = 0
+}
